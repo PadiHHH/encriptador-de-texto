37953ac8c9b18ccc6dbb4378a8580402c6c55306
Daniel Padilla <daniel.padilla.hinojosa@gmail.com>
2024-08-26 13:17:46 -0600

    Actualiza html, agrega variables a css y cambio los colores del sieño, actualiza js para funcionalidad del boton copiar, evaluador si el texto del usuario contiene minusculas o caracteres especiales, comentarios explicando cada funcion

--- app.js
+++ app.js
@@ -9,13 +9,39 @@ La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat" 
 */
 
-function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
-    mensaje.style.backgroundImage = "none";
+// Función para validar el texto que contenga letras minúsculas y sin caracteres especiales
+function validarTexto(texto) {
+    // Solo acepta letras minúsculas y espacios
+    const caracteresValidos = 'abcdefghijklmnñopqrstuvwxyz ';
+
+  for (let i = 0; i < texto.length; i++) {
+    if (caracteresValidos.includes(texto[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
+// Función cuando se hace click en el botón Encriptar: 
+// Desaparece los elementos de título, párrafo y la imagen
+// Aparecen los elementos del boton de copiar y el resultado de la encriptacion
+function btnEncriptar(){
+    if (validarTexto(textArea.value)) {
+        const textoEncriptado = encriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        textArea.value = "";
+        document.querySelector(".texto__output__titulo").style.display = "none";
+        document.querySelector(".texto__output__parrafo").style.display = "none";
+        document.querySelector(".seccion__output__imagen").style.display = "none";
+        document.querySelector(".boton__copiar").style.display = "block";
+        document.querySelector(".texto__output__textarea").style.display = "block";
+    } else {
+        alert("Por favor, solo ingrese letras minúsculas y sin caracteres especiales")
+    }
+    
+}
+
+// Función encargada de encriptar con los parámetros definidos por las instrucciones
 function encriptar(stringEncriptada){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
@@ -29,14 +55,28 @@ function encriptar(stringEncriptada){
     return stringEncriptada;
 }
 
-
+// Función cuando se hace click en el botón Desencriptar: 
+// Desaparece los elementos de título, párrafo y la imagen
+// Aparecen los elementos del boton de copiar y el resultado de la desencriptacion
 function btnDesencriptar(){
-    const textoEncriptado = desencriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
+    if (validarTexto(textArea.value)) {
+        const textoEncriptado = desencriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        textArea.value = "";
+        document.querySelector(".texto__output__titulo").style.display = "none";
+        document.querySelector(".texto__output__parrafo").style.display = "none";
+        document.querySelector(".seccion__output__imagen").style.display = "none";
+        document.querySelector(".boton__copiar").style.display = "block";
+        document.querySelector(".texto__output__textarea").style.display = "block";
+
+    } else {
+        alert("Por favor, solo ingrese letras minúsculas y sin caracteres especiales")
+    }
+    
 }
 
 
+// Función encargada de desencriptar con los parámetros definidos por las instrucciones
 function desencriptar(stringDesencriptada){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
@@ -48,4 +88,11 @@ function desencriptar(stringDesencriptada){
 
     }
     return stringDesencriptada;
+}
+
+
+// Función para copiar los elementos del textarea en el portapapeles del usuario
+function btnCopiar() {
+    let copiar = navigator.clipboard.writeText (mensaje.value);
+    return copiar ;
 }
